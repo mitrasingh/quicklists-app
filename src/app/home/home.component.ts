a28ac4435b931ec8fb6439a37ccd2c099e45eb37
mitrasingh <mitra.coder@gmail.com>
2024-12-21 17:27:13 -0500

    feat: import app modal, bind isOpen input from modal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { ModalComponent } from '../shared/ui/modal.component';
 
 @Component({
   selector: 'app-home',
-  template: ` <p>Hello world</p> `,
+  template: `
+    <header>
+      <h1>Quicklists</h1>
+      <button>Add Checklist</button>
+    </header>
+
+    <app-modal [isOpen]="false">
+      <ng-template>Test</ng-template>
+    </app-modal>
+  `,
+  imports: [ModalComponent],
 })
 export default class HomeComponent {}
