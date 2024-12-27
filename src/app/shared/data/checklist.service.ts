aeef693cc0be0b455dbf044058f82f28f384789f
mitrasingh <mitra.coder@gmail.com>
2024-12-27 11:04:22 -0500

    feat: subscribe to add$ and update with new checklists

--- src/app/shared/data/checklist.service.ts
+++ src/app/shared/data/checklist.service.ts
@@ -1,6 +1,7 @@
 import { computed, Injectable, signal } from '@angular/core';
 import { AddChecklist, Checklist } from '../interfaces/checklist';
 import { Subject } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 export interface ChecklistsState {
   checklists: Checklist[];
@@ -20,4 +21,35 @@ export class ChecklistService {
 
   // source which will allow us to emmit into stream
   add$ = new Subject<AddChecklist>();
+
+  constructor() {
+    // reducers
+    this.add$.pipe(takeUntilDestroyed()).subscribe((checklist) =>
+      this.state.update((state) => ({
+        ...state,
+        checklists: [...state.checklists, this.addIdToChecklist(checklist)],
+      }))
+    );
+  }
+
+  private addIdToChecklist(checklist: AddChecklist) {
+    return {
+      ...checklist,
+      id: this.generateSlug(checklist.title),
+    };
+  }
+
+  private generateSlug(title: string) {
+    let slug = title.toLowerCase().replace(/\s+/g, '-');
+
+    const matchingSlugs = this.checklists().find(
+      (checklist) => checklist.id === slug
+    );
+
+    if (matchingSlugs) {
+      slug = slug + Date.now().toString();
+    }
+
+    return slug;
+  }
 }
