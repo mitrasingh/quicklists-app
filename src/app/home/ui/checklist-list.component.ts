1bdfb610c545a2be4ad2af51995fc769a0a3a166
mitrasingh <mitra.coder@gmail.com>
2024-12-27 14:18:18 -0500

    feat: add structure to presentational component

---
+++ src/app/home/ui/checklist-list.component.ts
@@ -0,0 +1,8 @@
+import { Component, input } from '@angular/core';
+import { Checklist } from '../../shared/interfaces/checklist';
+
+@Component({
+  selector: 'app-checklist-list',
+  template: `<h1>Checklist list</h1>`,
+})
+export class ChecklistList {}
