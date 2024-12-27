584847ead113a97bec5cf1c65ff6c4c867833354
mitrasingh <mitra.coder@gmail.com>
2024-12-27 14:24:18 -0500

    feat: add control flow in template to loop through each checklist of the checklists input signal

--- src/app/home/ui/checklist-list.component.ts
+++ src/app/home/ui/checklist-list.component.ts
@@ -3,7 +3,12 @@ import { Checklist } from '../../shared/interfaces/checklist';
 
 @Component({
   selector: 'app-checklist-list',
-  template: `<h1>Checklist list</h1>`,
+  template: `
+    <h1>Checklist list</h1>
+    @for (checklist of checklists(); track checklist.id) {
+    <p>{{ checklist.title }}</p>
+    }
+  `,
 })
 export class ChecklistList {
   checklists = input.required<Checklist[]>();
