9b722dc9a63b5044f1d0a08c3f55e6d4cabc1c4b
mitrasingh <mitra.coder@gmail.com>
2024-12-29 08:59:08 -0500

    feat: inject checklist service and activatedroute, convert parammap to signal

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { ChecklistService } from '../shared/data/checklist.service';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-checklist',
   template: `<p>checklist</p>`,
 })
-export default class ChecklistComponent {}
+export default class ChecklistComponent {
+  checklistService = inject(ChecklistService);
+  route = inject(ActivatedRoute);
+
+  params = toSignal(this.route.paramMap);
+}
