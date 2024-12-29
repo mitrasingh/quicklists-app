3650b17c0de53f5131c4c2056ab05bfee19b6598
mitrasingh <mitra.coder@gmail.com>
2024-12-29 09:28:14 -0500

    feat: update template to conditionally render checklist header component using checklist data

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -2,10 +2,16 @@ import { Component, computed, inject } from '@angular/core';
 import { ChecklistService } from '../shared/data/checklist.service';
 import { ActivatedRoute } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { ChecklistHeaderComponent } from './ui/checklist-header.component';
 
 @Component({
   selector: 'app-checklist',
-  template: `<p>checklist</p>`,
+  template: `
+    @if (checklist(); as checklist) {
+    <app-checklist-header [checklist]="checklist" />
+    }
+  `,
+  imports: [ChecklistHeaderComponent],
 })
 export default class ChecklistComponent {
   checklistService = inject(ChecklistService);
