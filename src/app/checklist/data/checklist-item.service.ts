912f93e56c700bdbe4f1c60be965056d801a4b88
mitrasingh <mitra.coder@gmail.com>
2024-12-29 15:32:10 -0500

    feat: add state, selector and observable source

--- src/app/checklist/data/checklist-item.service.ts
+++ src/app/checklist/data/checklist-item.service.ts
@@ -1,6 +1,26 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
+import {
+  AddChecklistItem,
+  ChecklistItem,
+} from '../../shared/interfaces/checklist-item';
+import { Subject } from 'rxjs';
+
+export interface ChecklistItemsState {
+  checklistItems: ChecklistItem[];
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class ChecklistItemService {}
+export class ChecklistItemService {
+  // state
+  private state = signal<ChecklistItemsState>({
+    checklistItems: [],
+  });
+
+  // selectors
+  checklistItems = computed(() => this.state().checklistItems);
+
+  // sources
+  add$ = new Subject<AddChecklistItem>();
+}
