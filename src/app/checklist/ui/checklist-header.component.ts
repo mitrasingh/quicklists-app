fb88eb0dd7263c3c1bd8cb98d35f4955c7ef6b50
mitrasingh <mitra.coder@gmail.com>
2024-12-29 09:21:39 -0500

    feat: import router link, create checklist signal property, create template structure

--- src/app/checklist/ui/checklist-header.component.ts
+++ src/app/checklist/ui/checklist-header.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Checklist } from '../../shared/interfaces/checklist';
 
 @Component({
   selector: 'app-checklist-header',
-  template: `<p>checklist header</p>`,
+  template: `
+    <header>
+      <a routerLink="/home">Back</a>
+      <h1>{{ checklist().title }}</h1>
+    </header>
+  `,
+  imports: [RouterLink],
 })
-export class ChecklistHeaderComponent {}
+export class ChecklistHeaderComponent {
+  checklist = input.required<Checklist>();
+}
