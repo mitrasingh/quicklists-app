f0eec0cb8dd98a22006eebe36a823ea1b62a0762
mitrasingh <mitra.coder@gmail.com>
2024-12-29 09:45:27 -0500

    feat: update checklist list items to use router link

--- src/app/home/ui/checklist-list.component.ts
+++ src/app/home/ui/checklist-list.component.ts
@@ -1,17 +1,21 @@
 import { Component, input } from '@angular/core';
 import { Checklist } from '../../shared/interfaces/checklist';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-checklist-list',
   template: `
     <ul>
       @for (checklist of checklists(); track checklist.id) {
-      <li>{{ checklist.title }}</li>
+      <li>
+        <a routerLink="/checklist/{{ checklist.id }}">{{ checklist.title }}</a>
+      </li>
       } @empty {
       <p>Click the add button to create your first checklist!</p>
       }
     </ul>
   `,
+  imports: [RouterLink],
 })
 export class ChecklistList {
   checklists = input.required<Checklist[]>();
