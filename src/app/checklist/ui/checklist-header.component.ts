99cb79a8e1964f68c27dd67fbbb48128b81a2129
mitrasingh <mitra.coder@gmail.com>
2024-12-31 06:57:42 -0500

    feat: add output signal, add button in template to emit output

--- src/app/checklist/ui/checklist-header.component.ts
+++ src/app/checklist/ui/checklist-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Checklist } from '../../shared/interfaces/checklist';
 
@@ -8,10 +8,14 @@ import { Checklist } from '../../shared/interfaces/checklist';
     <header>
       <a routerLink="/home">Back</a>
       <h1>{{ checklist().title }}</h1>
+      <div>
+        <button (click)="addItem.emit()">Add item</button>
+      </div>
     </header>
   `,
   imports: [RouterLink],
 })
 export class ChecklistHeaderComponent {
   checklist = input.required<Checklist>();
+  addItem = output();
 }
