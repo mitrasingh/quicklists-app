274840c88d1a1c73632a3bd342870b7958aaeef0
mitrasingh <mitra.coder@gmail.com>
2024-12-31 07:11:45 -0500

    feat: add template structure to component to display check list items with conditional empty state message

--- src/app/checklist/ui/checklist-item-list.component.ts
+++ src/app/checklist/ui/checklist-item-list.component.ts
@@ -3,7 +3,24 @@ import { ChecklistItem } from '../../shared/interfaces/checklist-item';
 
 @Component({
   selector: 'app-checklist-item-list',
-  template: ` <div></div> `,
+  template: `
+    <section>
+      <ul>
+        @for (item of checklistItems(); track item.id) {
+        <li>
+          <div>
+            {{ item.title }}
+          </div>
+        </li>
+        } @empty {
+        <div>
+          <h2>Add an item</h2>
+          <p>Click the add button to add your first item to this quicklist</p>
+        </div>
+        }
+      </ul>
+    </section>
+  `,
 })
 export class ChecklistItemListComponent {
   checklistItems = input.required<ChecklistItem[]>();
