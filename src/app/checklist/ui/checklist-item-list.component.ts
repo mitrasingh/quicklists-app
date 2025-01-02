ce09c7f76377ac135ad5e6b57ee4aa607b33ff55
mitrasingh <mitra.coder@gmail.com>
2025-01-02 08:01:21 -0500

    feat: add output signal of type RemovelistItem

--- src/app/checklist/ui/checklist-item-list.component.ts
+++ src/app/checklist/ui/checklist-item-list.component.ts
@@ -1,5 +1,8 @@
-import { Component, input } from '@angular/core';
-import { ChecklistItem } from '../../shared/interfaces/checklist-item';
+import { Component, input, output } from '@angular/core';
+import {
+  ChecklistItem,
+  RemoveChecklistItem,
+} from '../../shared/interfaces/checklist-item';
 
 @Component({
   selector: 'app-checklist-item-list',
@@ -24,4 +27,5 @@ import { ChecklistItem } from '../../shared/interfaces/checklist-item';
 })
 export class ChecklistItemListComponent {
   checklistItems = input.required<ChecklistItem[]>();
+  toggle = output<RemoveChecklistItem>();
 }
