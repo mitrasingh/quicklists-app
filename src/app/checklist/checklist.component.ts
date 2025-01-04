36e166f648da8cfcd1da8de4272850c036c4b514
mitrasingh <mitra.coder@gmail.com>
2025-01-04 17:27:23 -0500

    feat: add resetChecklist event binding to trigger all checked items within list to be of false

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -17,6 +17,7 @@ import { ChecklistItemListComponent } from './ui/checklist-item-list.component';
     <app-checklist-header
       [checklist]="checklist"
       (addItem)="checklistItemBeingEdited.set({})"
+      (resetChecklist)="checklistItemService.reset$.next($event)"
     />
     }
     <app-checklist-item-list
