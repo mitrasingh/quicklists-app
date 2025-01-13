6cc0c1cc9679c7abb9e5ec32676e728871c3e1ba
mitrasingh <mitra.coder@gmail.com>
2025-01-13 18:12:09 -0500

    feat: add styles to component

--- src/app/checklist/ui/checklist-item-list.component.ts
+++ src/app/checklist/ui/checklist-item-list.component.ts
@@ -33,6 +33,27 @@ import {
       </ul>
     </section>
   `,
+  styles: [
+    `
+      ul {
+        padding: 0;
+        margin: 0;
+      }
+      li {
+        font-size: 1.5em;
+        display: flex;
+        justify-content: space-between;
+        background: var(--color-light);
+        list-style-type: none;
+        margin-bottom: 1rem;
+        padding: 1rem;
+
+        button {
+          margin-left: 1rem;
+        }
+      }
+    `,
+  ],
 })
 export class ChecklistItemListComponent {
   checklistItems = input.required<ChecklistItem[]>();
