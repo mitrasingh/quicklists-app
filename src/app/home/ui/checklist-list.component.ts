999efd5509283abb5cf30b97c767abc9087e6807
mitrasingh <mitra.coder@gmail.com>
2025-01-13 17:59:39 -0500

    feat: styles property added for styling of component

--- src/app/home/ui/checklist-list.component.ts
+++ src/app/home/ui/checklist-list.component.ts
@@ -19,6 +19,27 @@ import { RouterLink } from '@angular/router';
       }
     </ul>
   `,
+  styles: [
+    `
+      ul {
+        padding: 0;
+        margin: 0;
+      }
+      li {
+        font-size: 1.5em;
+        display: flex;
+        justify-content: space-between;
+        background: var(--color-light);
+        list-style-type: none;
+        margin-bottom: 1rem;
+        padding: 1rem;
+
+        button {
+          margin-left: 1rem;
+        }
+      }
+    `,
+  ],
   imports: [RouterLink],
 })
 export class ChecklistList {
