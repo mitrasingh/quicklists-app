ca4404b28ce6c81a1d3c59a802ff0b2cbbd25732
mitrasingh <mitra.coder@gmail.com>
2025-01-14 12:27:32 -0500

    feat: add date property to Checklist interface, add date to omit list for AddChecklist interface

--- src/app/shared/interfaces/checklist.ts
+++ src/app/shared/interfaces/checklist.ts
@@ -1,8 +1,9 @@
 export interface Checklist {
   id: string;
   title: string;
+  date: string;
 }
 
-export type AddChecklist = Omit<Checklist, 'id'>;
+export type AddChecklist = Omit<Checklist, 'id' | 'date'>;
 export type EditChecklist = { id: Checklist['id']; data: AddChecklist };
 export type RemoveChecklist = Checklist['id'];
